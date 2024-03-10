1ecc1b8c14478bd046cdc954947977502da8dffa
Evgenii Smirennikov <evgenii.smirennikov@gmail.com>
2024-03-10 19:06:51 +0300

    add private repository interfaces for store

--- internal/store/store.go
+++ internal/store/store.go
@@ -1,18 +1,32 @@
 package store
 
 import (
-	"test-crud-goods/internal/store/goods"
-	"test-crud-goods/internal/store/logs"
+	"context"
+	"test-crud-goods/internal/models"
 )
 
 type Store struct {
-	Goods *goods.Goods
-	Logs  *logs.Logs
+	Goods goodsRepository
+	Logs  logsRepository
 }
 
-func New(goods *goods.Goods, logs *logs.Logs) *Store {
+func New(goods goodsRepository, logs logsRepository) *Store {
 	return &Store{
 		Goods: goods,
 		Logs:  logs,
 	}
+}
+
+type logsRepository interface {
+	SendBatch(ctx context.Context, batchLogs []models.GoodLogEvent) error
+}
+
+type goodsRepository interface {
+	Create(ctx context.Context, good models.Good) (id *int, err error)
+
+	List(ctx context.Context, filter models.GoodsFilter, options models.GoodsOptions) (goods []models.Good, err error)
+	ListMeta(ctx context.Context) (meta models.GoodsMeta, err error)
+
+	Update(ctx context.Context, good models.Good) error
+	Reprioritize(ctx context.Context, curPriority, newPriority int) error
 }
